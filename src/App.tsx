e7e791bfda94274e581c1c7ff7d7e65a8f6f7759
seyit-ivhed <seyit.ivhed@gmail.com>
2025-12-04 11:49:08 +0100

    Remove the math sandbox page

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import HomePage from './pages/HomePage';
 import MissionPage from './pages/MissionPage';
 import ShipBayPage from './pages/ShipBayPage';
 import CombatPage from './pages/CombatPage';
-import MathSandboxPage from './pages/MathSandboxPage';
+
 import { CombatUITestPage } from './pages/CombatUITestPage';
 
 function App() {
@@ -14,7 +14,7 @@ function App() {
         <Route path="/mission-select" element={<MissionPage />} />
         <Route path="/ship-bay" element={<ShipBayPage />} />
         <Route path="/combat" element={<CombatPage />} />
-        <Route path="/sandbox" element={<MathSandboxPage />} />
+
         <Route path="/combat-ui-test" element={<CombatUITestPage />} />
       </Routes>
     </BrowserRouter>
--- src/components/Feedback/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as FeedbackIndicator } from './FeedbackIndicator';
-export type { FeedbackState } from './FeedbackIndicator';
