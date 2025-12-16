349d34f6ffea41778961a44a400d8f3f7f9eaa57
seyit-ivhed <seyit.ivhed@gmail.com>
2025-12-16 10:27:28 +0100

    feat: remove MathChallengeModal component and its styles, and update EncounterPage

--- src/features/encounter/EncounterPage.tsx
+++ src/features/encounter/EncounterPage.tsx
@@ -5,7 +5,7 @@ import { useEncounterStore } from '../../stores/encounter.store';
 import { useGameStore } from '../../stores/game.store';
 import { usePlayerStore } from '../../stores/player.store';
 import { EncounterPhase } from '../../types/encounter.types';
-// import MathChallengeModal from '../../components/MathChallengeModal'; // Removed
+
 import { generateProblem } from '../../utils/math-generator';
 import { MathOperation, type MathProblem } from '../../types/math.types';
 import { useState } from 'react';
