4b1725dc8b00a60d76aeeb8e0affe980006a8eda
seyit-ivhed <seyit.ivhed@gmail.com>
2025-12-16 10:31:14 +0100

    chore: remove MathInput component, including Numpad and MultipleChoice sub-components.

--- src/components/MathInput/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Numpad } from './Numpad';
-export { default as MultipleChoice } from './MultipleChoice';
-export { default as MathInput } from './MathInput';
-export type { InputMode } from './MathInput';
