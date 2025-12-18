73b81844c6d49d775e3890326f5dcbd419d72fb8
seyit-ivhed <seyit.ivhed@gmail.com>
2025-12-18 22:12:00 +0100

    feat: Add Zzz animation and indicator for resting units on UnitCard.

--- src/features/encounter/components/UnitCard.tsx
+++ src/features/encounter/components/UnitCard.tsx
@@ -167,6 +167,16 @@ export const UnitCard = ({
             }
 
             <UnitCardVFX activeVisualEffect={activeVisualEffect} />
+
+            {/* Sleeping/Resting State Indicator */}
+            {unit.hasActed && !unit.isDead && !isMonster && (
+                <div className="zzz-container">
+                    <div className="zzz-icon zzz-1">Z</div>
+                    <div className="zzz-icon zzz-2">Z</div>
+                    <div className="zzz-icon zzz-3">Z</div>
+                </div>
+            )}
+
         </div>
     );
 };
