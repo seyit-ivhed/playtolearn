1eb7b0a0a40efb0ce8420fac6a5819a5e8f00a1b
seyit-ivhed <seyit.ivhed@gmail.com>
2025-12-26 00:02:45 +0100

    test: add spiritGain property to test entities in player actions slice tests.

--- src/stores/encounter/slices/player-actions.slice.test.ts
+++ src/stores/encounter/slices/player-actions.slice.test.ts
@@ -61,8 +61,8 @@ describe('Player Actions Slice', () => {
 
     describe('performAction', () => {
         it('should deal damage for WARRIOR role', () => {
-            const warrior = { id: 'u1', templateId: 'warrior_id', name: 'Warrior', hasActed: false, maxHealth: 100, currentHealth: 100, isDead: false, isPlayer: true, maxShield: 0, currentShield: 0, currentSpirit: 0, maxSpirit: 100 };
-            const monster = { id: 'm1', templateId: 'goblin', name: 'Goblin', currentHealth: 50, maxHealth: 50, isDead: false, isPlayer: false, maxShield: 0, currentShield: 0, currentSpirit: 0, maxSpirit: 100, hasActed: false };
+            const warrior = { id: 'u1', templateId: 'warrior_id', name: 'Warrior', hasActed: false, maxHealth: 100, currentHealth: 100, isDead: false, isPlayer: true, maxShield: 0, currentShield: 0, currentSpirit: 0, maxSpirit: 100, spiritGain: 10 };
+            const monster = { id: 'm1', templateId: 'goblin', name: 'Goblin', currentHealth: 50, maxHealth: 50, isDead: false, isPlayer: false, maxShield: 0, currentShield: 0, currentSpirit: 0, maxSpirit: 100, hasActed: false, spiritGain: 10 };
 
             useEncounterStore.setState({ party: [warrior], monsters: [monster] });
 
@@ -78,8 +78,8 @@ describe('Player Actions Slice', () => {
 
     describe('resolveSpecialAttack', () => {
         it('should deal single target damage', () => {
-            const warrior = { id: 'u1', templateId: 'warrior_id', name: 'Warrior', hasActed: false, currentSpirit: 100, maxHealth: 100, currentHealth: 100, isDead: false, isPlayer: true, maxShield: 0, currentShield: 0, maxSpirit: 100 };
-            const monster = { id: 'm1', templateId: 'goblin', name: 'Goblin', currentHealth: 50, maxHealth: 50, isDead: false, isPlayer: false, maxShield: 0, currentShield: 0, currentSpirit: 0, maxSpirit: 100, hasActed: false };
+            const warrior = { id: 'u1', templateId: 'warrior_id', name: 'Warrior', hasActed: false, currentSpirit: 100, maxHealth: 100, currentHealth: 100, isDead: false, isPlayer: true, maxShield: 0, currentShield: 0, maxSpirit: 100, spiritGain: 10 };
+            const monster = { id: 'm1', templateId: 'goblin', name: 'Goblin', currentHealth: 50, maxHealth: 50, isDead: false, isPlayer: false, maxShield: 0, currentShield: 0, currentSpirit: 0, maxSpirit: 100, hasActed: false, spiritGain: 10 };
 
             useEncounterStore.setState({ party: [warrior], monsters: [monster] });
 
@@ -92,9 +92,9 @@ describe('Player Actions Slice', () => {
         });
 
         it('should deal AOE damage', () => {
-            const mage = { id: 'u1', templateId: 'aoe_dmg_id', name: 'Mage', hasActed: false, currentSpirit: 100, maxHealth: 100, currentHealth: 100, isDead: false, isPlayer: true, maxShield: 0, currentShield: 0, maxSpirit: 100 };
-            const m1 = { id: 'm1', templateId: 'g1', name: 'G1', currentHealth: 50, maxHealth: 50, isDead: false, isPlayer: false, maxShield: 0, currentShield: 0, currentSpirit: 0, maxSpirit: 100, hasActed: false };
-            const m2 = { id: 'm2', templateId: 'g2', name: 'G2', currentHealth: 30, maxHealth: 30, isDead: false, isPlayer: false, maxShield: 0, currentShield: 0, currentSpirit: 0, maxSpirit: 100, hasActed: false };
+            const mage = { id: 'u1', templateId: 'aoe_dmg_id', name: 'Mage', hasActed: false, currentSpirit: 100, maxHealth: 100, currentHealth: 100, isDead: false, isPlayer: true, maxShield: 0, currentShield: 0, maxSpirit: 100, spiritGain: 10 };
+            const m1 = { id: 'm1', templateId: 'g1', name: 'G1', currentHealth: 50, maxHealth: 50, isDead: false, isPlayer: false, maxShield: 0, currentShield: 0, currentSpirit: 0, maxSpirit: 100, hasActed: false, spiritGain: 10 };
+            const m2 = { id: 'm2', templateId: 'g2', name: 'G2', currentHealth: 30, maxHealth: 30, isDead: false, isPlayer: false, maxShield: 0, currentShield: 0, currentSpirit: 0, maxSpirit: 100, hasActed: false, spiritGain: 10 };
 
             useEncounterStore.setState({ party: [mage], monsters: [m1, m2] });
 
@@ -106,8 +106,8 @@ describe('Player Actions Slice', () => {
         });
 
         it('should apply AOE shield', () => {
-            const guardian = { id: 'u1', templateId: 'guardian_id_revived', name: 'Guardian', hasActed: false, currentShield: 0, currentSpirit: 100, maxHealth: 100, currentHealth: 100, isDead: false, isPlayer: true, maxShield: 0, maxSpirit: 100 };
-            const ally = { id: 'u2', templateId: 'warrior_id', name: 'Ally', hasActed: false, currentShield: 10, maxHealth: 100, currentHealth: 100, isDead: false, isPlayer: true, maxShield: 0, currentSpirit: 0, maxSpirit: 100 };
+            const guardian = { id: 'u1', templateId: 'guardian_id_revived', name: 'Guardian', hasActed: false, currentShield: 0, currentSpirit: 100, maxHealth: 100, currentHealth: 100, isDead: false, isPlayer: true, maxShield: 0, maxSpirit: 100, spiritGain: 10 };
+            const ally = { id: 'u2', templateId: 'warrior_id', name: 'Ally', hasActed: false, currentShield: 10, maxHealth: 100, currentHealth: 100, isDead: false, isPlayer: true, maxShield: 0, currentSpirit: 0, maxSpirit: 100, spiritGain: 10 };
 
             useEncounterStore.setState({ party: [guardian, ally], monsters: [] });
 
@@ -123,7 +123,7 @@ describe('Player Actions Slice', () => {
 
 
         it('should handle failure (drain spirit, end turn)', () => {
-            const warrior = { id: 'u1', templateId: 'warrior_id', name: 'Warrior', hasActed: false, currentSpirit: 100, maxHealth: 100, currentHealth: 100, isDead: false, isPlayer: true, maxShield: 0, currentShield: 0, maxSpirit: 100 };
+            const warrior = { id: 'u1', templateId: 'warrior_id', name: 'Warrior', hasActed: false, currentSpirit: 100, maxHealth: 100, currentHealth: 100, isDead: false, isPlayer: true, maxShield: 0, currentShield: 0, maxSpirit: 100, spiritGain: 10 };
             useEncounterStore.setState({ party: [warrior], monsters: [] });
 
             useEncounterStore.getState().resolveSpecialAttack('u1', false);
