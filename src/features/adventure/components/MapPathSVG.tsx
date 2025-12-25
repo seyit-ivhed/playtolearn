f49d3de96b60f56270b26aaf03aa7d4aa912adcb
seyit-ivhed <seyit.ivhed@gmail.com>
2025-12-25 23:08:31 +0100

    feat: simplify map path styling by removing gradient and adjusting stroke properties

--- src/features/adventure/components/MapPathSVG.tsx
+++ src/features/adventure/components/MapPathSVG.tsx
@@ -7,12 +7,6 @@ interface MapPathSVGProps {
 export const MapPathSVG: React.FC<MapPathSVGProps> = ({ encounters }) => {
     return (
         <svg className="map-svg-path" viewBox="0 0 500 4500" preserveAspectRatio="xMidYMin slice">
-            <defs>
-                <linearGradient id="pathGradient" x1="0%" y1="0%" x2="0%" y2="100%">
-                    <stop offset="0%" stopColor="#fbbf24" stopOpacity="0.2" />
-                    <stop offset="100%" stopColor="#fbbf24" stopOpacity="0.8" />
-                </linearGradient>
-            </defs>
             {/* Dynamic Path Logic based on encounter coordinates */}
             <path
                 d={encounters.reduce((acc, node, i) => {
