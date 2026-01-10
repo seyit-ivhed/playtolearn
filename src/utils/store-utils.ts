2a1141553c9be09d1c65cab0b147493ca38ebf98
seyit-ivhed <seyit.ivhed@gmail.com>
2026-01-10 20:13:27 +0100

    Remove obsolete store-utils.ts

--- src/utils/store-utils.ts
+++
@@ -1,8 +0,0 @@
-import { usePlayerStore } from '../stores/player.store';
-
-export const resetGame = () => {
-    usePlayerStore.getState().resetProgress();
-
-    // Force reload to ensure all transient state is cleared
-    window.location.reload();
-};
