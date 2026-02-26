df9f496958868cac5a3baf0efa571b6d856f5535
seyit-ivhed <seyit.ivhed@gmail.com>
2026-02-26 18:47:38 +0100

    Remove unnecessary code

--- src/utils/math/validation.ts
+++ src/utils/math/validation.ts
@@ -1,8 +1,5 @@
 import { type ValidationResult } from '../../types/math.types';
 
-/**
- * Validates a user's answer
- */
 export const validateAnswer = (
     userAnswer: number | string,
     correctAnswer: number | string
